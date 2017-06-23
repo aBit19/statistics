844df5d59b01e036716228909dac008b43e75496
abit <abit@itu.dk>
2017-06-24 00:27:22 +0200

    Implements Statistic service

--- src/main/scala/example/TrainingSetStats.scala
+++ src/main/scala/example/TrainingSetStats.scala
@@ -2,13 +2,24 @@ package example
 
 import scala.io.Source
 
-case class TrainingSetStats(inputFile: String, targetFile: String) {
-  private val inputLines = Source.fromFile(inputFile).getLines().toList
-  private val targetLines = Source.fromFile(targetFile).getLines().toList
-
+class TrainingSetStats(private val inputLines: List[String], private val targetLines:List[String]) {
   if (inputLines.size != targetLines.size)
-    throw new IllegalArgumentException()
+    throw new IllegalArgumentException
+  val pairs: Int = inputLines.size
+  val getSentenceLengthStats: Map[Int, Int] = {
+    (inputLines.groupBy(s => s.length) ++ targetLines.groupBy(s => s.length)).mapValues(s => s.size)
+  }
+  val mean: Double = (inputLines ++ targetLines).map(_.length).sum / (pairs * 2)
+  val std: Double = Math.sqrt((inputLines ++ targetLines).map(_.length).map(s => Math.pow(s - mean, 2)).sum / (pairs * 2))
+  val correct: Int = inputLines.zip(targetLines).count(s => s._1 == s._2)
+  val wrong: Int = pairs - correct
 
-  val pairs = inputLines.size
+  def <= (that: TrainingSetStats) : TrainingSetStats =
+    new TrainingSetStats(this.inputLines ++ that.inputLines, this.targetLines ++ that.targetLines)
+}
 
+object TrainingSetStats {
+  val f: String => List[String] = s => Source.fromFile(s).getLines().toList
+  def apply(inputPath: String, targetPath: String): TrainingSetStats =
+    new TrainingSetStats(f(inputPath), f(targetPath))
 }
--- src/test/scala/example/TrainingSetStatsSpec.scala
+++ src/test/scala/example/TrainingSetStatsSpec.scala
@@ -4,22 +4,61 @@ import java.io.File
 
 import org.scalatest._
 
-class TrainingSetStatsSpec extends FlatSpec {
+class TrainingSetStatsSpec extends FlatSpec with BeforeAndAfterEach {
   private val path = new File(".").getCanonicalFile + "/data/"
+  private val stats = TrainingSetStats(path + "input.txt", path + "targetValid.txt")
+
 
   "A TrainingStats" should "accept two files 1) input sentence and 2) target sentence respectively " in {
     val trainingSetStatsSpec = TrainingSetStats(path + "input.txt", path + "input.txt")
   }
 
-  it should "throw IllegalArgumentException in case the file have different lines of code" in {
+  it should "throw IllegalArgumentException in case the file have different number of lines" in {
     assertThrows[IllegalArgumentException]{
       val trainingSetStats = TrainingSetStats(path + "input.txt", path + "target.txt")
     }
   }
 
   it should "return number of sentence pairs" in {
-    val trainingSetStats = TrainingSetStats(path + "input.txt",  path + "targetValid.txt")
-    assert(trainingSetStats.pairs == 4)
+    assert(stats.pairs == 4)
+  }
+
+  it should "return statistics in Map object with the sentence length as key" in {
+    val map: Map[Int, Int] = stats.getSentenceLengthStats
+  }
+
+  it should "return 4 sentences with length 14" in {
+    assert(stats.getSentenceLengthStats.get(14).contains(4))
+  }
+
+  it should "return the mean of the sentence length" in {
+    assert(stats.mean == 14)
+  }
+
+  it should "return the stdv of the sentence length" in {
+    assert(stats.std == 0)
+  }
+
+  it should "return the number of correctinput" in {
+    assert(stats.correct == 4)
+  }
+
+  it should "return the number of errored" in {
+    assert(stats.wrong == 0)
+  }
+
+  it can "combine with another Stat" in {
+    val that = TrainingSetStats(path + "input.txt", path + "targetValid.txt")
+    val combine: TrainingSetStats = stats <= that
+    assert(combine.pairs == 2 * stats.pairs)
+    assert(combine.wrong == that.wrong * 2)
+    assert(combine.correct == that.correct * 2)
+    assert(combine.std == stats.std)
+    assert(combine.mean == combine.mean)
+  }
+
+  it can "process lang8 data" in {
+    val lang8 = TrainingSetStats(path + "entries.test.original", path + "entries.test.corrected")
   }
 
 }
